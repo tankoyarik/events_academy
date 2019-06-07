07b77ab3845c0a672a72529c3d0241ce6bb7f7dc
Yaroslav <tonkoyarik@gmail.com>
2019-06-07 14:53:15 +0300

    Added serializer

--- events_system/settings.py
+++ events_system/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'events'
 ]
 
@@ -73,8 +74,12 @@ WSGI_APPLICATION = 'events_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'events',
+        'USER': 'events_user',
+        'PASSWORD': 'asdfasdf',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
