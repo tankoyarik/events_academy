f5543897fcef66847d35a3ad0617aa43e611e896
Yaroslav <tonkoyarik@gmail.com>
2019-06-10 20:16:04 +0300

    Created endpoints to get event guests Added event guests view and serializers

--- events/serializers.py
+++ events/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
 
-from events.models import Event
+from events.models import Event, Guest
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = "__all__"
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = '__all__'
+
+
+class EventGuestsSerializer(serializers.ModelSerializer):
+    guests = GuestSerializer(many=True)
+
+    class Meta:
+        model = Event
+        fields = "__all__"
+
+
--- events/views.py
+++ events/views.py
@@ -4,9 +4,24 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from events.models import Event
-from events.serializers import EventSerializer
+from events.serializers import EventSerializer, EventGuestsSerializer
 
 
 class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventsGuestsList(generics.ListCreateAPIView):
+    queryset = Event.objects.prefetch_related('guests').all()
+    serializer_class = EventGuestsSerializer
+
+
+class EventGuests(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.prefetch_related('guests').all()
+    serializer_class = EventGuestsSerializer
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
