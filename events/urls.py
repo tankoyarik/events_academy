b144b87cacd7b97a5858d9b4c1ac1d2f73c5fbe7
Yaroslav <tonkoyarik@gmail.com>
2019-06-12 19:32:45 +0300

    Created API views to create, delete, retrieve event data

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from events.views import EventList
+from events.views import EventList, EventDetail, EventCreate
 
 urlpatterns = [
     path('events', EventList.as_view()),
+    path('events/<int:pk>', EventDetail.as_view()),
+    path('events/create', EventCreate.as_view())
     # path('events/<int:pk>/', my_view)
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from events.models import Event
 from events.serializers import EventSerializer
@@ -10,3 +13,34 @@ from events.serializers import EventSerializer
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class EventDetail(APIView):
+
+    def get(self, request, pk):
+        try:
+            event = Event.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response(data={"message": "Not found"}, status=404)
+        serializer = EventSerializer(instance=event)
+        return Response(data=serializer.data)
+
+
+    def delete(self, request, pk):
+        try:
+            event = Event.objects.get(id=pk).delete()
+        except Event.DoesNotExist:
+            return Response(data={"message": "Not found"}, status=404)
+
+        return Response(data={"message": "Object deleted successfully"}, status=204)
+
+class EventCreate(APIView):
+
+    def post(self, request):
+        payload = request.data
+        serializer = EventSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=201)
+        else:
+            return Response(data={"message":"Invalid"}, status=400)
