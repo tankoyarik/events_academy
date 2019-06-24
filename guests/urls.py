f66f16fbebdc367303dbfc164d9f235f9ca4910b
BerkutRuslan <berkutruslan1@gmail.com>
2019-06-24 19:31:12 +0300

    removed guests app

--- guests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from events.views import ,
-
-#(PK = ID)
-urlpatterns = [
-    path('guests', EventList.as_view()),
-    path('guests/<int:pk>', EventDetail.as_view())
-]
