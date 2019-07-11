b6df03536681ff0f6613ba00c1f2471bff640953
Yaroslav <tonkoyarik@gmail.com>
2019-07-11 17:38:18 +0300

    Changed ngins config

--- events_system/settings.py
+++ events_system/settings.py
@@ -158,3 +158,25 @@ EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_CONFIRMATION = env("EMAIL_CONFIRMATION")
 USE_X_FORWARDED_HOST = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
